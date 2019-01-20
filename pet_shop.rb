9a7083dd10c2353ba2892b8410dbc19aec10a55f
dvioletta <Anita@Pixie.local>
2019-01-20 15:28:06 +0000

    options

--- pet_shop.rb
+++ pet_shop.rb
@@ -77,3 +77,14 @@ def add_pet_to_customer(customer, new_pet)
    customer[:pets] << new_pet
    return customer[:pets].count
 end
+
+#Optionals
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] < new_pet[:price]
+    return false
+  else
+    return true 
+    end
+    #return true
+  end
